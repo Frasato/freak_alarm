c488d0539fb8a6b019e921d6a913a394b8720069
Frasato <gabriel.frasato@hotmail.com>
2026-04-14 18:26:10 -0300

    feat: alarm receiver to take the response of the messages for android

---
+++ app/src/main/java/com/frasato/freakalarm/data/receiver/AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package com.frasato.freakalarm.data.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class AlarmReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?){
+        Log.d("ALARM_DEBUG", "ALARM WORKING")
+    }
+}
