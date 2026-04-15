ace86ab7724c44b1dbdc56c326a251f362074d32
Frasato <gabriel.frasato@hotmail.com>
2026-04-15 16:37:57 -0300

    feat: create alarm screen to create a screen

---
+++ app/src/main/java/com/frasato/freakalarm/ui/alarm/AlarmScreen.kt
@@ -0,0 +1,42 @@
+package com.frasato.freakalarm.ui.alarm
+
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
+
+@Composable
+fun AlarmScreen(viewModel: AlarmViewModel){
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                if (!viewModel.canScheduleExactAlarms(context)) {
+                    val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                        data = "package:${context.packageName}".toUri()
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    context.startActivity(intent)
+                } else {
+                    viewModel.scheduleTestAlarm()
+                }
+            }
+        ) {
+            Text("Agendar alarm (1 min)")
+        }
+    }
+}
