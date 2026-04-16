2271c9ee86a26971737050d5662485f357494b4a
Frasato <gabriel.frasato@hotmail.com>
2026-04-16 16:06:28 -0300

    feat: Create time picker and button to set alarm

--- app/src/main/java/com/frasato/freakalarm/ui/alarm/AlarmScreen.kt
+++ app/src/main/java/com/frasato/freakalarm/ui/alarm/AlarmScreen.kt
@@ -1,42 +1,42 @@
 package com.frasato.freakalarm.ui.alarm
 
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.net.toUri
+import androidx.navigation.NavController
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AlarmScreen(viewModel: AlarmViewModel){
-    val context = LocalContext.current
+fun AlarmScreen(
+    viewModel: AlarmViewModel,
+    navController: NavController
+){
+    val timeState = rememberTimePickerState()
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        TimePicker(state = timeState)
         Button(
             onClick = {
-                if (!viewModel.canScheduleExactAlarms(context)) {
-                    val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
-                        data = "package:${context.packageName}".toUri()
-                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    }
-                    context.startActivity(intent)
-                } else {
-                    viewModel.scheduleAlarm()
-                }
+                viewModel.scheduleAlarm(timeState.hour, timeState.minute)
             }
-        ) {
-            Text("Agendar alarm (1 min)")
-        }
+        ) { Text("Programar Alarm") }
+
+        Button(
+            onClick = {
+                navController.popBackStack()
+            }
+        ) { Text("Voltar") }
     }
 }
